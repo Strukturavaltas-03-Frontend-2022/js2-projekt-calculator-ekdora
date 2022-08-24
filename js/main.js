a91d0eb38f11d1300d161505ced9459e7a6f40f7
ekdora <dora.egrykiss@gmail.com>
2022-08-24 19:25:50 +0200

    Some improvements

--- js/main.js
+++ js/main.js
@@ -7,19 +7,17 @@ const doOp = {
 	'-': (a, b) => a - b,
 	'x': (a, b) => a * b,
 	'/': (a, b) => a / b,
-}
+};
 
 const calcResult = (typedString) => {
 	const typedNums = typedString.split(/\+|-|x|\//);
 	const firstTypedNum = typedNums[0];
-	if(!firstTypedNum) {
-		typedNums[0] = '0';
+	if (!firstTypedNum) {
+		firstTypedNum = '0';
 	}
-	const typedOps = typedString.split('').filter((char) => ['+', '-', 'x', '/'].includes(char));
+	const typedOps = typedString.split('').filter(char => /\+|-|x|\//.test(char));
 	const nums = typedNums.map((typedNum) => parseFloat(typedNum));
-	let result = 0;
-	console.log(typedNums, typedOps, nums);
-	result = nums[0];
+	let result = nums[0];
 	for (let i = 1; i < nums.length; i++) {
 		const num = nums[i];
 		result = doOp[typedOps[i - 1]](result, num);
@@ -33,8 +31,10 @@ const eventListenerFactory = (htmlElements) => {
 		element.addEventListener(
 			'click',
 			() => {
-				if (screenInput.length < 24 ) {
-					const selectedBtnSign = element.innerHTML;
+				const selectedBtnSign = element.innerHTML;
+				const isLastCharSpec = screenInput.length === 23 && /\.|\+|-|x|\//.test(selectedBtnSign);
+				
+				if (screenInput.length < 24 && !isLastCharSpec || selectedBtnSign === 'C' || selectedBtnSign === '=') {
 					const prevChar = screenInput?.charAt(screenInput.length - 1);
 					const beforePrevChar = screenInput.length > 1 && screenInput?.charAt(screenInput.length - 2);
 					const lastTwoCharZero = prevChar === '0' && selectedBtnSign === '0';
