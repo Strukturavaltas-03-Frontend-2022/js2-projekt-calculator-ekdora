616d49bbb1d1fce2ad0fd0c715195347eba03367
ekdora <dora.egrykiss@gmail.com>
2022-08-28 23:10:52 +0200

    Some correction

--- js/main.js
+++ js/main.js
@@ -6,16 +6,16 @@ const doOp = {
 	'+': (a, b) => a + b,
 	'-': (a, b) => a - b,
 	'x': (a, b) => a * b,
-	'/': (a, b) => a / b,
+	'÷': (a, b) => b ? a / b : 'Nullával nem lehet osztani',
 };
 
 const calcResult = (typedString) => {
-	const typedNums = typedString.split(/\+|-|x|\//);
+	const typedNums = typedString.split(/\+|-|x|÷/);
 	const firstTypedNum = typedNums[0];
 	if (!firstTypedNum) {
 		typedNums[0] = '0';
 	}
-	const typedOps = typedString.split('').filter(char => /\+|-|x|\//.test(char));
+	const typedOps = typedString.split('').filter(char => /\+|-|x|÷/.test(char));
 	const nums = typedNums.map((typedNum) => parseFloat(typedNum));
 	let result = nums[0];
 	for (let i = 1; i < nums.length; i++) {
@@ -32,13 +32,13 @@ const eventListenerFactory = (htmlElements) => {
 			'click',
 			() => {
 				const selectedBtnSign = element.innerHTML;
-				const isLastCharSpec = screenInput.length === 23 && /\.|\+|-|x|\//.test(selectedBtnSign);
-				
+				const isLastCharSpec = screenInput.length === 23 && /\.|\+|-|x|÷/.test(selectedBtnSign);
+
 				if (screenInput.length < 24 && !isLastCharSpec || selectedBtnSign === 'C' || selectedBtnSign === '=') {
 					const prevChar = screenInput?.charAt(screenInput.length - 1);
 					const beforePrevChar = screenInput.length > 1 && screenInput?.charAt(screenInput.length - 2);
 					const lastTwoCharZero = prevChar === '0' && selectedBtnSign === '0';
-					const afterOpMoreZeros = beforePrevChar && /\+|-|x|\//.test(beforePrevChar) && lastTwoCharZero;
+					const afterOpMoreZeros = beforePrevChar && /\+|-|x|÷/.test(beforePrevChar) && lastTwoCharZero;
 					const afterFirstZeroMoreZeros = screenInput.length === 1 && lastTwoCharZero;
 
 					if (!afterOpMoreZeros && !afterFirstZeroMoreZeros) {
@@ -50,9 +50,9 @@ const eventListenerFactory = (htmlElements) => {
 							screenInput = '';
 							calcInput.value = '';
 						} else if (selectedBtnSign !== '=') {
-							const hasError = /(\.|\+|-|x|\/)[^0-9]/.test(`${prevChar}${selectedBtnSign}`)
-								|| !screenInput && /\.|\+|x|\//.test(selectedBtnSign);
-							
+							const hasError = /(\.|\+|-|x|÷)[^0-9]/.test(`${prevChar}${selectedBtnSign}`)
+								|| !screenInput && /\.|\+|x|÷/.test(selectedBtnSign);
+
 							if (!hasError) {
 								screenInput += selectedBtnSign;
 								calcInput.value = screenInput;
